83c12c6ee94d23ad9b80a96c2b3687c37cd77cb5
Nathaniel Escano <205729250+Natex15@users.noreply.github.com>
2026-04-23 14:41:56 +0800

    style: Improved Login CSS

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import './LoginPage.css';
 
 export default function LoginPage() {
     const [password, setPassword] = useState("");
@@ -19,16 +20,19 @@ export default function LoginPage() {
     };
 
     return (
-        <>
-            <h1>Login</h1>
+        <div className="login-container">
+            <h1 className="login-title">LTO IMS</h1>
+            <h1 className="login-title2">ADMIN LOGIN</h1>
             <input
+                className="password-input"
                 onChange={e => setPassword(e.target.value)}
                 placeholder="Password"
                 type="password"
                 value={password}
             />
-            <button onClick={login}>Login</button>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-        </>
+            <button className="login-button" onClick={login}>Login</button>
+            {error && <p className="login-error">{error}</p>}
+            {!error && <p className="login-error" style={{color: '#1E1E1E'}}>No Error</p>}
+        </div>
     );
 }
